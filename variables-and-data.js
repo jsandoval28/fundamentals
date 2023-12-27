c6de994e91fe75e38dde6098a40869c66c15e04f
Jacob Sandoval <jsandoval283@cnm.edu>
2023-12-27 15:14:00 -0700

    Added conditionals-and-functions.js file & objects-apis.js file & variables-and-data.js file

---
+++ variables-and-data.js
@@ -0,0 +1,51 @@
+/*
+Variables
+ */
+
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subtotal = 100;
+let tax= subtotal * 0.07;
+let totalWithTax = tax + subtotal;
+
+/*
+Data Types
+ */
+
+let obj = {
+    key1: "Value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1: "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2Key1);
+
+/*
+Typing
+ */
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2
+
+console.log(result);
+
+    /*
+    Operators
+     */
+
+    let add = 1 + 2;
+    let sub = 2 - 1;
+    let mult = 2 * 4;
+    let division = 4 / 2;
+    let mod = 5 % 2;
+
+    let string = "String 1 " + "String 2";
+
+    console.log(string);
